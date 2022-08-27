649d7677ed97048b65845342783dbce1508b2ea8
RoyOkite <royokite@gmail.com>
2022-08-28 01:47:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+          .then(response => response.json())
+          .then(books => renderBooks(books));
   
 }
 
@@ -7,9 +9,17 @@ function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHTML = `${book.name}: <em>${book.numberOfPages} pages</em>`;
     main.appendChild(h2);
   });
+  const totalPages = document.createElement('h3');
+  totalPages.innerHTML = `<em style='color: red'>Total Pages: ${books.map(book=>book.numberOfPages).reduce((a, c)=>a+c)}</em>`;
+  main.appendChild(totalPages);
+
+  const fifthBook = document.createElement('h3');
+  fifthBook.innerHTML = `<em style='color: blue'>Fifth Book: ${books[4].name}</em>`;
+  main.appendChild(fifthBook);
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
